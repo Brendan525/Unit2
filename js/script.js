8957aa464061ad473616dc2aededf1e193c96f62
Brendan <broe5252@hotmail>
2018-11-14 23:38:32 -0500

    update

--- js/script.js
+++ js/script.js
@@ -6,8 +6,44 @@ var results;
 
 
 if(eventType === "casual" && tempFahr < 54){
-results = `Since it is ${tempFahr} degrees and you are going to a ${eventType} event. You should ware something comfy and a coat`;
+results = `Since it is ${tempFahr} degrees and you are going to a ${eventType} event. You should wear something comfy and a coat`;
 console.log (results);
 }
 
-  
+else if(eventType === "casual" && tempFahr >= 54 && eventType === "casual" && tempFahr <= 70){   
+result = `Since it is ${tempFahr} degrees and you are going to a  ${eventType} event. You should wear something comfy and a jacket`;
+console.log (results);
+}
+
+else if(eventType === "casual" && tempFahr > 70){
+result = `Since it is ${tempFahr} degrees and you are going to a ${eventType} event. You should wear a something comfy and no jacket.`
+console.log(result);
+}
+
+if(eventType === "semi-casual" && tempFahr < 54){
+results = `Since it is ${tempFahr} degrees and you are going to a ${eventType} event. You should wear a polo and a coat`;
+console.log (results);
+}
+
+else if(eventType === "semi-casual" && tempFahr >= 54 && eventType === "semi-casual" && tempFahr <= 70){   
+result = `Since it is ${tempFahr} degrees and you are going to a  ${eventType} event. You should wear a polo and a jacket`;
+console.log (results);
+}
+
+else if(eventType === "semi-casual" && tempFahr > 70){
+ result = `Since it is ${tempFahr} degrees and you are going to a ${eventType} event. You should wear a polo and no jacket.`
+console.log(result);
+
+if(eventType === "formal" && tempFahr < 54){
+results = `Since it is ${tempFahr} degrees and you are going to a ${eventType} event. You should wear a suit and a coat`;
+console.log (results);
+}
+
+else if (eventType === "formal" && tempFahr >= 54 && eventType === "formal" && tempFahr <= 70){   
+result = `Since it is ${tempFahr} degrees and you are going to a  ${eventType} event. You should wear a suit and a jacket`;
+console.log (results);
+}
+
+else if (eventType === "formal" && tempFahr > 70){
+result = `Since it is ${tempFahr} degrees and you are going to a ${eventType} event. You should wear a suit and no jacket.`
+console.log(result);
